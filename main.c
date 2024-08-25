f47aafddf41bad28c5fdcaf2c419f89c9390bd56
Ellen Marie Dash <me@duckie.co>
2024-08-25 17:59:43 -0400

    ?????

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <stdio.h>      // for printf() and friends
 #include <unistd.h>     // for close()
+#include <signal.h>
 #include <sys/epoll.h>  // for epoll_*
 #include <sys/socket.h> // for... socket... stuff...
 #include <stdlib.h>     // for EXIT_FAILURE, EXIT_SUCCESS
@@ -13,6 +14,19 @@
 static int BACKLOG = 50;
 static int PORT = 8080;
 
+static int done = false;
+void prepare_to_exit(int _signal) {
+    done = true;
+}
+
+void register_signal_handler(void) {
+    struct sigaction sigact;
+    sigact.sa_handler = prepare_to_exit;
+    sigemptyset(&sigact.sa_mask);
+    sigact.sa_flags = 0;
+    sigaction(SIGINT, &sigact, (struct sigaction *)NULL);
+}
+
 void watch_socket(int epoll_fd, int sock_fd) {
     struct epoll_event event;
     event.events = EPOLLIN | EPOLLET;
@@ -20,21 +34,20 @@ void watch_socket(int epoll_fd, int sock_fd) {
 
     if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_fd, &event)) {
         perror("epoll_ctl");
-        //close(epoll_fd);
     }
 }
 
-int server_socket() {
+int server_socket(void) {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         perror("socket");
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     if (fcntl(server_fd, F_SETFL, O_NONBLOCK) == -1) {
         perror("fcntl");
         close(server_fd);
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     struct sockaddr_in server = {0};
@@ -44,12 +57,12 @@ int server_socket() {
 
     if (bind(server_fd, (struct sockaddr*)&server, sizeof(server))) {
         perror("bind");
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     if (listen(server_fd, BACKLOG)) {
         perror("listen");
-        exit(EXIT_FAILURE);
+        return -1;
     }
 
     return server_fd;
@@ -62,7 +75,7 @@ int main(int argc, char *argv[]) {
     puts("");
 
     int epoll_fd = epoll_create1(0);
-    printf("got epoll_fd\n");
+    puts("Got epoll_fd.");
 
     if (epoll_fd == -1) {
         perror("epoll_create1");
@@ -70,16 +83,27 @@ int main(int argc, char *argv[]) {
     }
 
     int server_fd = server_socket();
-    printf("got a server socket\n");
+    if (server_fd == -1) {
+        return EXIT_FAILURE;
+    }
+    puts("Got server socket.");
+
+    register_signal_handler();
+    puts("Registered signal handlers.");
 
     struct epoll_event events[MAX_EVENTS] = {0};
     watch_socket(epoll_fd, server_fd);
-    printf("watching server_fd\n");
-    while (true) {
-        printf("epoll_wait()...\n");
+    puts("Watching server_fd.");
+    while (!done) {
+        puts("epoll_wait()...");
         int num_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (num_events < 0) {
+            perror("epoll_wait");
+            break;
+        }
+
         for (size_t i = 0; i < num_events; i++) {
-            printf("Handling event %zu of %i\n", i + 1, num_events);
+            printf("  Handling event %zu of %i\n", i + 1, num_events);
             if ((events[i].events & EPOLLERR) ||
                 (events[i].events & EPOLLHUP) ||
                 (!(events[i].events & EPOLLIN))) {
@@ -90,11 +114,11 @@ int main(int argc, char *argv[]) {
             }
 
             if (server_fd == events[i].data.fd) {
-                printf("Handling server socket.\n");
+                printf("  Handling server socket.\n");
                 while (true) {
-                    printf("accept()...\n");
+                    printf("    accept()...\n");
                     int client_fd = accept(server_fd, NULL, NULL);
-                    printf("got client_fd: %i\n", client_fd);
+                    printf("    got client_fd: %i\n", client_fd);
                     if (client_fd == -1) {
                         // EAGAIN/EWOULDBLOCK aren't actual errors, so
                         // be quiet about it.
@@ -103,18 +127,27 @@ int main(int argc, char *argv[]) {
                         break;
                     }
 
+                    if (fcntl(client_fd, F_SETFL, O_NONBLOCK) == -1) {
+                        perror("fcntl");
+                        close(client_fd);
+                        continue;
+                    }
+
                     watch_socket(epoll_fd, client_fd);
                 }
                 continue;
             }
 
-            printf("Handling client socket.\n");
+            puts("  Handling client socket.");
             while (true) {
                 char buf[512];
+                puts("    Reading...");
                 ssize_t count = read(events[i].data.fd, buf, sizeof buf);
+                printf("    Read %zu bytes.\n", count);
 
                 // read() failed
                 if (count == -1) {
+                    perror("client read()");
                     if (errno != EAGAIN) {
                         perror("read");
                         close(events[i].data.fd);
@@ -124,6 +157,7 @@ int main(int argc, char *argv[]) {
 
                 // there's nothing left to read.
                 if (count == 0) {
+                    puts("    Sending response!");
                     char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 37\r\nConnection: close\r\n\r\n<!doctype html>\r\n<p>hello, world!</p>";
                     size_t length = strlen(response);
                     if (send(events[i].data.fd, response, length, 0) == -1)
@@ -135,9 +169,22 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    puts("Marking address as reusable.");
+    int optval = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int))) {
+        perror("setsockopt");
+    }
+
+    // Closing the server socket will cause the main event loop
+    // to eventually terminate.
+    puts("Closing server socket.");
+    if (close(server_fd)) {
+        perror("close");
+    }
+
+    puts("Closing epoll socket.");
     if (close(epoll_fd)) {
         perror("close(epoll_fd)");
-        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
